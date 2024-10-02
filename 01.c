aa41ecf24454ce415883ecc42d2823eb4617e64f
Rafa <rafasnna@gmail.com>
2024-10-02 11:17:02 -0300

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,22 @@
+/*
+Author: Rafael Sampaio
+Para compilar em terminal (janela de comandos):
+Linux : gcc -o exemplo0100 exemplo0100.c
+Windows: gcc -o exemplo0100 exemplo0100.c
+Para executar em terminal (janela de comandos):
+Linux : ./exemplo0100
+Windows: exemplo0100
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+int Inteiro = 0;
+
+
+
+
+}
