e25044dadb9c339a00005e871f6d581ca52fafba
Rafael Sampaio <rafasnna@gmail.com>
2025-03-26 23:48:10 -0300

    26/03/2025

--- 866307_RafaelSampaio/ED05/ATV_05.c
+++ 866307_RafaelSampaio/ED05/ATV_05.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "io.h"
+#include <math.h>
 
 /* 01.) Incluir função e método (0511) para: 
 para ler uma quantidade inteira (n) do teclado e, mediante um procedimento, 
@@ -28,12 +29,9 @@ int n = 0;
 
     n = IO_readint("Entre com um inteiro: ");
     mult3(n);
-    IO_pause("Pressione <enter> para finalizar");
+    IO_pause("Pressione <enter> para continuar");
 }
-int main() {
-    at01();
-    return 0;
-}
+
 
 
  
@@ -43,7 +41,28 @@ testar a função para gerar valores múltiplos de 3 e 5 em ordem crescente.
  
 Exemplo:  n = 5 => { 15, 30, 45, 60, 75 } */
 
+void mult3e5 (int n){
 
+    int multiplos = 15;
+    
+    for (int i = 0; i < n; i++){
+        if(i > 0){
+            IO_printf(",");
+        }   
+        IO_printf("%d", multiplos);
+        multiplos += 15;
+    }
+        IO_printf("\n");
+    }
+
+void at02 (){
+
+int n = 0;
+    n = IO_readint("Entre com um inteiro: ");
+    mult3e5(n);
+
+    IO_pause("Pressione <enter> para continuar");
+}
 
  
 /* 03.) Incluir função e método (0513) para: 
@@ -52,6 +71,31 @@ testar a função para gerar potências de 4 em ordem decrescente.
  
 Exemplo:  n = 5 => { 1024, 256, 64, 16, 4 } */
 
+void pot4 (int n){
+
+int potencia = 1;
+
+    for(int i = 0; i < n; i++){ // potencia 4
+    potencia *= 4;
+    }
+    for(int i = n; i > 0; i--){ // formula descrescente
+        if(i < n){
+            IO_printf(",");
+        }
+        IO_printf("%d", potencia);
+        potencia /= 4;
+    }
+    IO_printf("\n");
+}
+
+void at03(){
+
+int n = 0;
+    n = IO_readint("Entre com um inteiro: ");
+    pot4(n);
+
+    IO_pause("Pressione <enter> para continuar");
+}
 
 
  
@@ -62,6 +106,28 @@ testar a função para gerar valores crescentes nos denominadores
  
 Exemplo:  n = 5 => { 1/3, 1/6, 1/9, 1/12, 1/15 } */
 
+void invmult3(int n){
+
+int denom = 3; //denominador
+
+    for (int i = 0; i < n; i++){//i é igual a 0, e i é menor que n, entao adicione 1 toda vez que rodar
+        if(i > 0){// colocar virgula
+            IO_printf(", ");
+        }
+        IO_printf("1/%d", denom);
+        denom += 3;
+    }
+    IO_printf("\n");
+}
+
+void at04(){
+
+int n = 0;
+    n = IO_readint("Entre com um valor inteiro desejado: ");
+    invmult3(n);
+
+    IO_printf("Pressione <enter> para continuar");
+}
 
 
  
@@ -74,8 +140,34 @@ DICA: Usar da biblioteca <math.h> a função pow ( x, y ) para calcular a potên
  
 Exemplo:  n = 5 => { 1, 1/x2, 1/x4, 1/x6, 1/x8 } */
 
+void seq (int n, double x){
 
+    IO_printf("Sequencia: ");
 
+    for(int i = 0; i < n; i++){
+        if(i > 0){
+        IO_printf(", ");
+        }
+        if(i == 0){
+        IO_printf("1");
+        }else{
+            double denom = pow(x, 2* i);
+            IO_printf("1/%.2f", denom);
+        }
+    }
+    IO_printf("\n");
+}
+
+void at05(){
+
+double x = 0.0;
+int    n = 0;
+
+       x = IO_readdouble("Entre com o valor real: ");// vezes que voce quer apareça
+       n = IO_readint("Entre com a quantidade de termos: "); //e sua potencia
+       seq(x,n);
+    IO_pause("Pressione <enter> para continuar.");
+}
  
 /* 06.) Incluir função e método (0516) para 
 calcular a soma dos primeiros valores positivos começando no valor 3, 
@@ -84,9 +176,37 @@ Testar essa função para quantidades diferentes e mostrar os resultados em outr
  
 Exemplo:  n = 5 => 3 + 6 + 9 + 15 + 18 */
 
+void sm3nm4 (int n){
+    
+int contador = 0;
+int numero   = 3;
+int soma     = 0;
 
+    IO_printf("Valores: ");
 
- 
+    while (contador < n){
+        if(numero % 3 == 0 && numero % 4 !=0){
+            if(contador > 0){
+                IO_printf(" + ");
+            }
+            IO_printf("%d", numero);
+            soma += numero;
+            contador++;
+        }
+        numero++;
+    }
+    IO_printf(" = %d\n", soma);
+}
+
+void at06 (){
+
+int n = 0;
+
+    n = IO_readint("Entre com a quantidade de valores: ");
+        sm3nm4(n);
+
+    IO_printf("Pressione <enter> para continuar");
+} 
  
  
 /* 07.) Incluir função e método (0517) para 
@@ -155,3 +275,41 @@ ler uma quantidade inteira do teclado (n) e,
 mediante o uso da função, calcular e mostrar o resultado em outro método de 
  
 f ( n ) = (1+2/3!) * (1+4/5!) * (1+6/7!) * ... */
+
+
+
+//MAIN
+int main (){
+
+int opcao = 0;
+    
+    IO_id("Programa - v0.0");
+       do
+       {
+        // ler opcao do teclado
+        printf ( "%s", "Escolha a opcao desejada? " );
+        scanf  ( "%d", &opcao ); getchar( );
+        printf ( "%s%d\n", "opcao = ", opcao );
+        
+        switch ( opcao )
+        {
+           case  0:            break;
+           case  1: at01 ( ); break;
+           case  2: at02 ( ); break;
+           case  3: at03 ( ); break;
+           case  4: at04 ( ); break;
+           case  5: at05 ( ); break;
+           case  6: at06 ( ); break;
+           //case  7: at07 ( ); break;
+           //case  8: at08 ( ); break;
+           //case  9: at09 ( ); break;
+           //case 10: at10 ( ); break;
+           default: printf ( "\n%s\n",
+                           "ERRO: Desisto\n\n" );
+            break;
+        }
+       } while ( opcao != 0 );
+    printf("%s\n", "\nApertar ENTER para terminar\n");
+    getchar( );
+	return ( 0 );
+}
